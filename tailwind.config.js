8d41d1261c5522cf31e9cc2d202bf0fbdaf38fa3
KevsOno <mickevsono@gmail.com>
2025-08-03 13:21:53 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./portfolio/index.html",
-    "./portfolio/**/*.html"
+    "./portfolio/**/*.{html,js}",
+    "./index.html"
   ],
   theme: {
     extend: {},
